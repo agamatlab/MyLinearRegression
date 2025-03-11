1c7193de6b3f31d18868e7fe946ccf183d3a41c3
agamatlab <icloudakberzade@gmail.com>
2025-03-11 19:56:34 -0400

    added visualization to the classifications

--- main.py
+++ main.py
@@ -9,62 +9,37 @@ with open('StudentsPerformance.csv', mode='r') as file:
     for row in csv_reader:
         data_list.append(row)
 
-mathPass = 0
-mathFail = 0
+mathPass = []
+mathFail = []
 
 for data in data_list:
     if(int(data["math score"]) > 60):
-        mathPass += 1
+        mathPass.append(data)
     else:
-        mathFail += 1
+        mathFail.append(data)
 
-print(mathPass)
-print(mathFail)
-
-
-# # Sample data
-# subjects = ['Math', 'Reading', 'Writing']
-# stock_A = [120, 125, 130, 135, 128]
-# stock_B = [60, 62, 128, 140, 145]
-# stock_C = [80, 85, 83, 88, 92]
-# stock_D = [200, 210, 215, 220, 230]
+print(len(mathPass))
+print(len(mathFail))
 
 
+# Sample data
+subjects = ["None", "Completed"]
+stock_A = [120, 125, 130, 135, 128]
+stock_B = [60, 62, 128, 140, 145]
+stock_C = [80, 85, 83, 88, 92]
+stock_D = [200, 210, 215, 220, 230]
 
-# # Setting positions for each bar
-# x = np.arange(len(subjects))
+completed = [data for data in data_list if data["test preparation course"] == "completed"]
+none = [data for data in data_list if data["test preparation course"] == "none"]
+priorCourse = ['None','Completed']
 
-# # Creating a figure with 4 subplots (2x2 layout)
-# fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+pasedList = [sum(1 for data in none if int(data["math score"]) > 60), sum(1 for data in completed if int(data["math score"]) > 60)]
+failedList = [1000-value for value in pasedList] 
+plt.bar(priorCourse, pasedList, color='g')
+plt.bar(priorCourse, failedList, bottom=pasedList, color='r')
+plt.title('Prior Courses')
+plt.xlabel('Prior Courses')
+plt.ylabel('Count')
+plt.show()
 
-# # Subplot 1: Stock A
-# axes[0, 0].bar(x, stock_A, color='skyblue')
-# axes[0, 0].set_title('Test Preparation Course')
-# axes[0, 0].set_xticks(x)
-# axes[0, 0].set_xticklabels(subjects)
 
-# axes[0, 0].bar(x - 0.2, , 0.4, label = 'Pass') 
-# axes[0, 0].bar(x + 0.2, Zboys, 0.4, label = 'Fail') 
-
-# # Subplot 2: Stock B
-# axes[0, 1].bar(x, stock_B, color='orange')
-# axes[0, 1].set_title('Stock B')
-# axes[0, 1].set_xticks(x)
-# axes[0, 1].set_xticklabels(subjects)
-
-# # Subplot 2: Stock C
-# axes[1, 0].bar(x, stock_C, color='green')
-# axes[1, 0].set_title('Stock C')
-# axes[1, 0].set_xticks(x)
-# axes[1, 0].set_xticklabels(subjects)
-
-# # Subplot 3: Stock D (example extra data)
-# axes[1, 1].bar(x, [115, 118, 123, 127, 130], color='skyblue')
-# axes[1, 1].set_title('Stock D')
-# axes[1, 1].set_xticks(x)
-# axes[1, 1].set_xticklabels(subjects)
-
-# # Add main title and adjust spacing
-# fig.suptitle('Monthly Stock Prices', fontsize=16)
-# fig.tight_layout(pad=2.0)
-# plt.show()
